87307325bc391121f38964b0d1b162d8e685cdfa
Anjalisharma3463 <anjalisharma1562005@gmail.com>
2025-03-16 17:46:57 +0530

    deleted env

--- src/server.js
+++ src/server.js
@@ -17,16 +17,15 @@ app.use(express.json());
 app.use(json());
 
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin:process.env.FRONTEND_URL, credentials: true }));
  
 app.use("/uploads", express.static(path.join("uploads")));
     
   
 app.use("/api/admin", adminRoutes);
 app.use("/api/user", userRoutes);
-
-const PORT = 4000;  
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+ 
+app.listen(process.env.PORT, () => console.log(`✅ Server running on port ${process.env.PORT}`));
 
  
  
